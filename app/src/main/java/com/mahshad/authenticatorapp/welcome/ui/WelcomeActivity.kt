b99ba5fed3b3418be6442b897d60c9dfe88d425c
mahshad76 <mahshadmahdavimoghadam@gmail.com>
2025-07-08 18:43:29 -0400

    cleaning the order of calling the functions in the onCreate of the activity

--- app/src/main/java/com/mahshad/authenticatorapp/welcome/ui/WelcomeActivity.kt
+++ app/src/main/java/com/mahshad/authenticatorapp/welcome/ui/WelcomeActivity.kt
@@ -7,18 +7,24 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.mahshad.authenticatorapp.R
 import com.mahshad.authenticatorapp.common.AppCompatActivityExtensions.replaceFragment
+import com.mahshad.authenticatorapp.databinding.ActivityMainBinding
 import com.mahshad.authenticatorapp.welcome.ui.login.LoginFragment
 
 class WelcomeActivity : AppCompatActivity() {
+    private lateinit var activityHomeBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        replaceFragment(R.id.fragment_container, LoginFragment())
-        setContentView(R.layout.activity_main)
+        activityHomeBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityHomeBinding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        if (savedInstanceState == null) {
+            replaceFragment(R.id.fragment_container, LoginFragment())
         }
     }
 }
