9f3494f24c3184aea1145b4c72c65db23e011065
mahshad76 <mahshadmahdavimoghadam@gmail.com>
2025-07-08 18:55:16 -0400

    the loginFragment and its inner views are extracted as objects

--- app/src/main/java/com/mahshad/authenticatorapp/welcome/ui/login/LoginFragment.kt
+++ app/src/main/java/com/mahshad/authenticatorapp/welcome/ui/login/LoginFragment.kt
@@ -4,10 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import androidx.fragment.app.Fragment
-import com.mahshad.authenticatorapp.R
+import com.mahshad.authenticatorapp.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
+
+    private lateinit var loginFragment: FragmentLoginBinding
+    private lateinit var usernameText: EditText
+    private lateinit var passwordText: EditText
+    private lateinit var loginButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +24,10 @@ class LoginFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        loginFragment = FragmentLoginBinding.inflate(inflater)
+        usernameText = loginFragment.usernameEditText
+        passwordText = loginFragment.passwordEditText
+        loginButton = loginFragment.myGradientMaterialButton
+        return loginFragment.root ?: null
     }
 }
