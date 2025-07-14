53c10d0773d1a0692da628dae6de83ab572e6b46
mahshad76 <mahshadmahdavimoghadam@gmail.com>
2025-07-14 15:06:26 -0400

    implementing the HasAndroidInjector by myapplication

--- app/src/main/java/com/mahshad/authenticatorapp/MyApplication.kt
+++ app/src/main/java/com/mahshad/authenticatorapp/MyApplication.kt
@@ -3,12 +3,21 @@ package com.mahshad.authenticatorapp
 import android.app.Application
 import com.mahshad.authenticatorapp.di.AppComponent
 import com.mahshad.authenticatorapp.di.DaggerAppComponent
+import dagger.android.AndroidInjector
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.HasAndroidInjector
+import javax.inject.Inject
 
-class MyApplication : Application() {
+class MyApplication : Application(), HasAndroidInjector {
     lateinit var appComponent: AppComponent
+
+    @Inject
+    lateinit var androidInjector: DispatchingAndroidInjector<Any>
 
     override fun onCreate() {
         super.onCreate()
         appComponent = DaggerAppComponent.factory().create(applicationContext)
     }
+
+    override fun androidInjector(): AndroidInjector<Any> = androidInjector
 }
