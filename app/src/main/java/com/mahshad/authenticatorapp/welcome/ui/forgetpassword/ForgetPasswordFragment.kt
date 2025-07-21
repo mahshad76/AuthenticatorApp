bb7cbaf560873e9882058a67a9ed85c7caa8635b
mahshad76 <mahshadmahdavimoghadam@gmail.com>
2025-07-21 18:21:23 -0400

    the forgot password layout is done

--- app/src/main/java/com/mahshad/authenticatorapp/welcome/ui/forgetpassword/ForgetPasswordFragment.kt
+++ app/src/main/java/com/mahshad/authenticatorapp/welcome/ui/forgetpassword/ForgetPasswordFragment.kt
@@ -4,26 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
 import androidx.fragment.app.Fragment
-import com.jakewharton.rxbinding3.view.clicks
-import com.jakewharton.rxbinding3.widget.textChanges
-import com.mahshad.authenticatorapp.databinding.FragmentSignUpBinding
-import io.reactivex.Observable
+import com.mahshad.authenticatorapp.databinding.FragmentForgetPasswordBinding
 
 class ForgetPasswordFragment : Fragment() {
-    private lateinit var signUpFragment: FragmentSignUpBinding
-    private lateinit var usernameEditText: EditText
-    private lateinit var usernameEditTextObservable: Observable<CharSequence>
-    private lateinit var fullNameEditText: EditText
-    private lateinit var fullNameEditTextObservable: Observable<CharSequence>
-    private lateinit var passwordEditText: EditText
-    private lateinit var passwordEditTextObservable: Observable<CharSequence>
-    private lateinit var phoneEditText: EditText
-    private lateinit var phoneEditTextObservable: Observable<CharSequence>
-    private lateinit var signUpButton: Button
-    private lateinit var signUpButtonObservable: Observable<Unit>
+    private lateinit var fragmentForgetPasswordBinding: FragmentForgetPasswordBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,17 +18,7 @@ class ForgetPasswordFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        signUpFragment = FragmentSignUpBinding.inflate(inflater)
-        usernameEditText = signUpFragment.usernameEditText
-        usernameEditTextObservable = usernameEditText.textChanges()
-        fullNameEditText = signUpFragment.fullNameEditText
-        fullNameEditTextObservable = fullNameEditText.textChanges()
-        passwordEditText = signUpFragment.passwordEditText
-        passwordEditTextObservable = passwordEditText.textChanges()
-        phoneEditText = signUpFragment.phoneEditText
-        phoneEditTextObservable = phoneEditText.textChanges()
-        signUpButton = signUpFragment.myGradientMaterialButton
-        signUpButtonObservable = signUpButton.clicks()
-        return signUpFragment.root
+        fragmentForgetPasswordBinding = FragmentForgetPasswordBinding.inflate(layoutInflater)
+        return fragmentForgetPasswordBinding.root
     }
 }
