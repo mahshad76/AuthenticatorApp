e9644a222737e90fa661ce37c00f0d659e1195a4
mahshad76 <mahshadmahdavimoghadam@gmail.com>
2025-07-25 14:37:33 -0400

    the adapter and the view holder classes are initialized

---
+++ app/src/main/java/com/mahshad/authenticatorapp/home/ui/home/ArticleAdapter.kt
@@ -0,0 +1,12 @@
+package com.mahshad.authenticatorapp.home.ui.home
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.mahshad.authenticatorapp.home.data.home.model.repository.Article
+
+class ArticleAdapter(private val articleList: List<Article>) {
+}
+
+class ArticleViewHolder(article: View) : RecyclerView.ViewHolder(article) {
+
+}
