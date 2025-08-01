8c6fde97dd2fd0e3b1aa702d946e9193c8d84982
mahshad76 <mahshadmahdavimoghadam@gmail.com>
2025-08-01 10:32:22 -0400

    the adapter is set

--- app/src/main/java/com/mahshad/authenticatorapp/home/ui/favorite/FavoriteAdapter.kt
+++ app/src/main/java/com/mahshad/authenticatorapp/home/ui/favorite/FavoriteAdapter.kt
@@ -1,26 +1,37 @@
 package com.mahshad.authenticatorapp.home.ui.favorite
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.mahshad.authenticatorapp.databinding.FavoriteArticleLayoutBinding
 import com.mahshad.authenticatorapp.home.data.favorite.model.FavoriteArticle
 
 class FavoriteAdapter(private val favoriteArticles: List<FavoriteArticle>) :
     RecyclerView.Adapter<ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
+        val binding = FavoriteArticleLayoutBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        return ViewHolder(binding)
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount(): Int = favoriteArticles.size
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.title.text = favoriteArticles[position].title
+        Glide.with(holder.image.context)
+            .load(favoriteArticles[position].urlToImage)
+            .into(holder.image)
     }
 }
 
 class ViewHolder(favoriteArticleLayoutBinding: FavoriteArticleLayoutBinding) :
     RecyclerView.ViewHolder(favoriteArticleLayoutBinding.root) {
-
+    val image: ImageView = favoriteArticleLayoutBinding.cardImage
+    val title: TextView = favoriteArticleLayoutBinding.cardTitle
 }
